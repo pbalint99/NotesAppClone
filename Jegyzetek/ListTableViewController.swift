c5a854af0f65fe91815749a3b798888fc4bf8496
Péter Bálint <pbalint999@gmail.com>
2018-08-14 12:23:02 +0200

    Prettier

--- Jegyzetek/ListTableViewController.swift
+++ Jegyzetek/ListTableViewController.swift
@@ -7,17 +7,15 @@
 //
 
 import UIKit
+import ChameleonFramework
 
 class ListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        navigationController?.navigationBar.tintColor = UIColor.white
+        
     }
 
     override func didReceiveMemoryWarning() {
