c343049c9fe8666c87e0bc63b49661a2c29e751e
Péter Bálint <pbalint999@gmail.com>
2018-08-14 13:25:45 +0200

    Segue initialised.

---
+++ Jegyzetek/Item.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  Jegyzetek
+//
+//  Created by Péter Bálint on 2018. 08. 14..
+//  Copyright © 2018. Péter Bálint. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Item: Object {
+    
+    @objc dynamic var title : String = ""
+    
+}
--- Jegyzetek/ListTableViewController.swift
+++ Jegyzetek/ListTableViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 import ChameleonFramework
 
+
+
 class ListTableViewController: UITableViewController {
+    
+    var itemArray = [Item]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,32 +22,24 @@ class ListTableViewController: UITableViewController {
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return itemArray.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
         return cell
+        
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -90,4 +86,18 @@ class ListTableViewController: UITableViewController {
     }
     */
 
+    @IBAction func addButton(_ sender: UIBarButtonItem) {
+        
+        performSegue(withIdentifier: "goToNote", sender: self)
+        
+    }
+    
+        
+
+        
+
+
+    
+    
+    
 }
